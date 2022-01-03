abbec323af3a92f92e880b68c90e2d96dddb47e9
afeiship <1290657123@qq.com>
2022-01-03 22:57:09 +0800

    feat: use dart-sass

--- build/docs.js
+++ build/docs.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var gulp = require('gulp');
+  var sass = require('gulp-dart-sass');
   var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del'],
   });
@@ -9,7 +10,7 @@
   gulp.task('docs', function () {
     return gulp
       .src('docs/style.scss')
-      .pipe($.sass({ outputStyle: 'expanded' }).on('error', $.sass.logError))
+      .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
       .pipe(gulp.dest('docs'));
   });
 })();
